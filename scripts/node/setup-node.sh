981b14f305bb6d4df864010c8c707e65d2288ccf
gbalnis <gbalnis@gmail.com>
2015-04-25 19:55:22 +0200

    new

---
+++ scripts/node/setup-node.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# /usr/local/bin must be on the secure_paths for sudo user
+# sudo vi /etc/sudoers
+# change: Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+# install pre-requisites
+sudo yum install gcc-c++ make
+sudo yum install git
+
+# install node.js
+git clone git://github.com/joyent/node.git
+cd node
+# git checkout v0.12.2 (see current version on https://nodejs.org/download/)
+./configure
+make
+sudo make install
+
+# install npm
+git clone https://github.com/isaacs/npm.git
+cd npm
+sudo make install
+
